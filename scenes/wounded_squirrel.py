62dce4b2bfdb3eff574b4380ab7264a6f7f7ecbb
Alex Poulidis <alepou@uni-bremen.de>
2026-03-26 15:24:01 +0100

    DEV: Wrote first two scene in squirrel story

--- scenes/wounded_squirrel.py
+++ scenes/wounded_squirrel.py
@@ -12,38 +12,43 @@ from utils import sleep
 # ===== Scene =======================================================
 
 @register    # You need to register the entry scene of your arc (and add the module to the scenes.__init__.py file.
-def example_scene():
+def getting_late():
     
-    print(f"\nYou can write some text like this")
-    sleep(2.0) # Add some suspense by adding breaks between text (this would add a 2 seconds break)
-    print("There can also be a break in the same line", end=' ', flush=True)
-    sleep(2.0)
-    print("like this!")
+    print(f"\nThe sun has begun to set...")
     sleep(1.0)
+    print(f"\nYou feel tired and start looking for a place to rest.")
+#    sleep(2.0) # Add some suspense by adding breaks between text (this would add a 2 seconds break)
+#    print("There can also be a break in the same line", end=' ', flush=True)
+#    sleep(2.0)
+#    print("like this!")
+#    sleep(1.0)
 
     # You can add options for the player to choose from like this:
     next_scene = choose_option([
-        ('option 1', continuation_scene), # If you want option 1 to lead to a continuation_scene
-        ('option 2', None)                # If you want option 2 to exit this scene and continue the game
+        ('Look for a tree', tree_scene), # If you want option 1 to lead to a continuation_scene
+        ('Look for a cave', cave_scene)                # If you want option 2 to exit this scene and continue the game
     ])
 
     return next_scene
 
 # -----------------------------------------------
 
-def continuation_scene():
+def tree_scene():
     
-    print(f"\nThis is an example continuation scene")
+    print(f"\nYou look for a suitably protected place within the forest...")
     sleep(1.0)
 
-    example_roll = chance_roll(25) # You can use a chance roll like this, where 25 is the probability of success
+    found_tree = chance_roll(25) # You can use a chance roll like this, where 25 is the probability of success
     
-    if example_roll:
+    if found_tree:
         
-        print(f"\nThings are happening!")
+        print(f"\nYou find a small secluded grove.")
         sleep(1.0)
+	print(f"\nAs you begin to make your bedroll, you hear a small noise...")
 
-        return None
+	next_scene = found_squirrel
+
+        return next_scene
         # When you are ready to end the scene, return:
         #   None:  to continue the game
         #   True:  to win the game
@@ -51,4 +56,10 @@ def continuation_scene():
     
     else:
 
-        return None
+	print(f"\nYou can't find a good place.")
+	next_scene = choose_option([
+		('Continue looking for a tree', monster_scene)
+		('Switch to looking for a cave', cave_scene)
+	])
+
+	return next_scene
